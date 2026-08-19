8d8dcd824fd0246dd0d51423bdc005af4a4061ea
Levi Cole <31482349+thelevicole@users.noreply.github.com>
2026-08-19 12:12:04 +0100

    Adds Meta Tracking Pixel

---
+++ tracking.js
@@ -0,0 +1,21 @@
+/* Facebook Pixel Code */
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '921578981865536');
+fbq('track', "PageView");
+/* End Facebook Pixel Code */
+
+document.addEventListener('click', function (event) {
+  const element = event.target.closest('[data-register-link]');
+
+  if (element) {
+    fbq('track', 'CompleteRegistration', {
+      registration_name: 'Rows & Columns Summit',
+      registration_id: 'rowsandcolumnssummit'
+    });
+  }
+}, true);
